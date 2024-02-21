3792193a5ddb072ac548f05e9dae14d54f600387
dhruva-star88 <dhruvatej6565@gmail.com>
2024-02-21 20:48:07 +0530

    Initial commit

---
+++ extract_image.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def get_image():
+    api_key = "nGkCgrdTCt8hO5IBDjwwi75tXLcznjrUJF8SNlug"
+    url = "https://api.nasa.gov/planetary/apod" \
+          f"?api_key={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    img_url = data["hdurl"]
+    img_content = data["explanation"]
+
+    image_response = requests.get(img_url)
+    image_binary_data = image_response.content
+
+    with open("image.jpg", "wb") as image:
+        image.write(image_binary_data)
+
+    return img_content
+
+
+def get_title():
+    api_key = "nGkCgrdTCt8hO5IBDjwwi75tXLcznjrUJF8SNlug"
+    url = "https://api.nasa.gov/planetary/apod" \
+          "?api_key=nGkCgrdTCt8hO5IBDjwwi75tXLcznjrUJF8SNlug"
+    response = requests.get(url)
+    data = response.json()
+    img_title = data["title"]
+    return img_title
---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from extract_image import get_image, get_title
+
+
+st.title(get_title())
+st.write("")
+st.image("image.jpg")
+
+st.write(get_image())
+
